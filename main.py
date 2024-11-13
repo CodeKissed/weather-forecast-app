a4d5f6ee1299296edc1b57bb2491f8e86d7269b7
Andromeda Canete-Borys <andycan@student.ubc.ca>
2024-11-13 11:36:42 -0800

    Added backend script to filter api data per user selection.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
+
 
 st.title("Weather Forecast for Next Days")
 place = st.text_input("Place: ")
@@ -9,4 +12,16 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} days in {place.title()}")
 
+data = get_data(place, days, option)
+
+def get_data(days):
+    dates =["2024-10-10", "2024-10-11", "2024-10-12"]
+    temperatures =[10, 15, 12]
+    temperatures = [days * i for i in temperatures]
+    return dates,temperatures
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels ={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
 
